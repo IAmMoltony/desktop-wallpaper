05601019b216b2b9d75ef047fafae3cfd9057806
IAmMoltony <koronavirusnyj@gmail.com>
2024-05-31 22:43:19 +0500

    Add make-symlinks.sh script

---
+++ make-symlinks.sh
@@ -0,0 +1,69 @@
+#!/bin/sh
+
+SCRIPTDIR=$(dirname "$0")
+
+make_dwp_symlink() {
+    ln -vs "$SCRIPTDIR/$1/$2" ./$2
+}
+
+make_dwp_symlink amogus amogus-black.png
+make_dwp_symlink amogus amogus-blue.png
+make_dwp_symlink amogus amogus-brown.png
+make_dwp_symlink amogus amogus-cyan.png
+make_dwp_symlink amogus amogus-green.ong
+make_dwp_symlink amogus amogus-holidays.png
+make_dwp_symlink amogus amogus-lime.png
+make_dwp_symlink amogus amogus-orange.ong
+make_dwp_symlink amogus amogus-pink.png
+make_dwp_symlink amogus amogus-purple.png
+make_dwp_symlink amogus amogus-white.png
+make_dwp_symlink amogus amogus-yellow.png
+make_dwp_symlink amogus amogus.png
+
+make_dwp_symlink arch arch-centerlogo-noneofetch.png
+make_dwp_symlink arch arch-centerlogo.png
+make_dwp_symlink arch arch-noneofetch.png
+make_dwp_symlink arch arch-notext-noneofetch.png
+make_dwp_symlink arch arch-notext.png
+make_dwp_symlink arch arch.png
+
+make_dwp_symlink azubananya bananya_real.png
+
+make_dwp_symlink babanya babanya.png
+
+make_dwp_symlink bash bash.png
+make_dwp_symlink bash bash_light.png
+
+make_dwp_symlink casa casa.png
+
+make_dwp_symlink cat cat.png
+
+make_dwp_symlink circles circles.png
+
+make_dwp_symlink grub grub.png
+
+make_dwp_symlink hjkl hjkl.png
+
+make_dwp_symlink linuh linuh.png
+
+make_dwp_symlink moltonies moltonies.png
+
+make_dwp_symlink moonlight moonlight.png
+
+make_dwp_symlink n n.png
+
+make_dwp_symlink refind refind.png
+
+make_dwp_symlink slg silly-little-guy.png
+
+make_dwp_symlink sunny cloudy.png
+make_dwp_symlink sunny sunny.png
+make_dwp_symlink sunny sunny_nocloud.png
+
+make_dwp_symlink watermelon-cat watermelon-cat.png
+
+make_dwp_symlink waves waves.png
+
+make_dwp_symlink windowz windowz.png
+
+make_dwp_symlink yp-gradient yellow-purple-gradient.png
