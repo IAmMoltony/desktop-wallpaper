3f8ebeb5b39afe5f0c44464433ff2c448b3c2932
IAmMoltony <koronavirusnyj@gmail.com>
2024-08-28 16:14:57 +0500

    Add coolpattern wp

---
+++ coolpattern/README.md
@@ -0,0 +1,5 @@
+# coolpattern
+
+a pattern i find pretty cool ngl
+
+based on `fedora_coolbg` background except i added some fx
--- make-symlinks.sh
+++ make-symlinks.sh
@@ -56,6 +56,8 @@ make_dwp_symlink chairtabletony chairtabletony_night_eyebrows.png
 
 make_dwp_symlink circles circles.png
 
+make_dwp_symlink coolpattern coolpattern.png
+
 make_dwp_symlink deertony deertony.png
 make_dwp_symlink deertony deertony_shadow.png
 make_dwp_symlink deertony deertony_shikatony.png
